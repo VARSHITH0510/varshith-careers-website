35b145befde1d673d73062fb5c6bc7f62dbbbbf5
VARSHITH0510 <135054452+VARSHITH0510@users.noreply.github.com>
2023-06-06 21:49:56 +0000

    added job details

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Hyderabad, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remort'
+ },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  } 
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS, company_name='Varshith')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
